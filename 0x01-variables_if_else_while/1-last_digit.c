a06082bb894c5b30e364f4c3064a8c08e2d88f1e
Tijani Olanrewaju <tijanigbolahangbolahan32@gmail.com>
2022-09-09 19:23:58 -0700

    last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,19 +4,19 @@
 /**
  * main - Entry point
  *
- * Return - Always 0 (Success)
+ * Return: (0)
  */
-int main(void) 
+int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
+	if (n < 5)
+		printf("%d and is greater than 5\n", n);
 	else if (n == 0)
-		printf("%d is zero\n", n);
+		printf("%d and is 0\n", n);
 	else
-		printf("%d is negative\n", n);
+		printf("%d and is less than 6\n", n);
 	return (0);
 }
