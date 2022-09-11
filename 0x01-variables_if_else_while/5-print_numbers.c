8a21243fa865d8e1ec34c28da8404e12461b0f59
Tijani Olanrewaju <tijanigbolahangbolahan32@gmail.com>
2022-09-11 02:38:54 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int numb;
+
+	for (numb = 0; numb <= 9; numb++)
+		printf("%d", numb);
+	printf("\n");
+
+	return (0);
+}
