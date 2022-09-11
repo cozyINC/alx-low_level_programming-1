a4916871ee0093dcedb90f64a562ae2267b58be9
Tijani Olanrewaju <tijanigbolahangbolahan32@gmail.com>
2022-09-11 00:51:46 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
 /**
- * main - main block
- * Return : 0
+ * Description: main - main block
+ * Return: 0 if success
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
-printf(
