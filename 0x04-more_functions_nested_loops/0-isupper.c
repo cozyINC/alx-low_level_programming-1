5e20fe1432a6b1fd123c19cab67f0c7343300d67
Tijani Olanrewaju <tijanigbolahangbolahan32@gmail.com>
2022-09-15 07:52:25 -0700

    c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 
 /**
  * _idupper - function to check if character is uppercase
--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
  * _isdigit - check for a digit (0 to 9)
  *
