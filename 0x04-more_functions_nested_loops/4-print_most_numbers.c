fd291f6356887b79b2f6d16a56834fb6654aeb90
Tijani Olanrewaju <tijanigbolahangbolahan32@gmail.com>
2022-09-15 11:42:28 -0700

    m

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print number from 0 to 0 excluding 2 and 4
+ *
+ * Return: null return
+ */
+
+void print_most_numbers(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+		if (number != 2 && number != 4)
+			_putchar(number);
+	_putchar('\n)
+}
