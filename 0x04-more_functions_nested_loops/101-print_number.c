bda21c3aab3c900fd9eb6c031c0f63648f54242d
Tijani Olanrewaju <tijanigbolahangbolahan32@gmail.com>
2022-09-15 21:55:55 -0700

    c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,40 +5,22 @@
  */
 void print_number(int n)
 {
-	int num;
-	int factor = 0;
-	int i;
-	int digit;
-	int last_digit;
+	unsigned int num = n;
 
-	digit = digit_num(n);
-	if (digit == 0)
-		_putchar('0' + n);
-	else if (digit > 0)
+	/*first check if its negative*/
+	if (n < 0)
 	{
-		if (n < 0)
-			_putchar('-');
-		for (i = digit - 1; i >= 1; i--)
-		{
-			factor = ten_to_power(i);
-			num = (n / factor) % 10;
-			if (num < 0)
-				num = -num;
-			_putchar('0' + num);
-		}
-		if (n < 0)
-			last_digit = -(n % 10);
-		else
-			last_digit = n % 10;
-		_putchar('0' + last_digit);
+		_putchar('-');
+		num = -num;
 	}
+
+	/*print the first few digits*/
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	/*print the last digit*/
+	_putchar((num % 10) + 48);
 }
-/**
- * digit_num - find how many digits in a number
- * @n: the number to be check
- * Return: return the number of digit
- */
-int digit_num(int n)
 {
 	int count = 0;
 
@@ -48,18 +30,4 @@ int digit_num(int n)
 		n = n / 10;
 	}
 	return (count);
-}
-/**
- * ten_to_power - get the value of 10 to the n power
- * @n: the number of power
- * Return: return the result
- */
-int ten_to_power(int n)
-{
-	int result = 10;
-	int i;
-
-	for (i = 1; i < n; i++)
-		result = result * 10;
-	return (result);
 }
