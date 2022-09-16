3aee5481b9cf814889045087b9dddfd85948a6f8
Tijani Olanrewaju <tijanigbolahangbolahan32@gmail.com>
2022-09-15 21:36:48 -0700

    c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,11 @@ void print_most_numbers(void)
 {
 	int number;
 
-	for (number = 48; number < 58; number++)
+	for (number = 0; number <= 9; number++)
 	{
-		if (number != 50 && number != 52)
-		{
-			_putchar(number);
-		}
+		if (number == 2 || number == 4)
+			continue;
+		_putchar(number);
 	}
 	_putchar('\n')
 }
