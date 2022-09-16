b235c66ab429fbcd506152cdd0f17dd164630929
Tijani Olanrewaju <tijanigbolahangbolahan32@gmail.com>
2022-09-15 21:27:40 -0700

    c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
  * print_numbers - prints numbers from 0 to 9
  *
  * Return: null return
  */
+
 void print_numbers(void)
 {
 	int c = 0;
 
 	do {
-		_putchar(48 + c);
+		_putchar(c + 48);
 		c++;
 	} while (c >= 0 && num <= 9);
 	_putchar('\n');
