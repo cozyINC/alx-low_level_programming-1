3a710f23aa9ab5c0493dcdf13d3aa9a389e0fa89
Tijani Olanrewaju <tijanigbolahangbolahan32@gmail.com>
2022-09-16 09:55:08 -0700

    c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,32 +1,26 @@
-#include "main.h"
+#include"main.h"
+
 /**
- * print_number - prints an int
- * @n: number to tbe printed
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
+
 void print_number(int n)
 {
-	unsigned int i, j, count;
+	unsigned int num = n;
 
+	/*first check if its negative*/
 	if (n < 0)
 	{
-		_putchar(45);
-		i = n * -1;
-	}
-	else
-	{
-		i = n;
-	}
-	j = i;
-	count = 1;
-
-	while (j > 9)
-	{
-		j /= 10;
-		count *= 10;
+		_putchar('-');
+		num = -num;
 	}
 
-	for (count >= 1; count /= 10)
-	{
-		_putchar(((i / count) % 10) + 48);
-	}
+	/*print the first few digits*/
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	/*print the last digit*/
+	_putchar((num % 10) + 48);
 }
