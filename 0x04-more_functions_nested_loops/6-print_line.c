3af442618e68c9e30dd170832bb52ea26dad947c
Tijani Olanrewaju <tijanigbolahangbolahan32@gmail.com>
2022-09-15 20:59:45 -0700

    c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,10 +7,12 @@ void print_line(int n)
 {
 	int line;
 
-	if (n > 0)
+	if (n <= 0)
+		_putchar('\n');
+	else
 	{
-		for (line = 0; line < n; line++)
-			_putchar('_')
+		for (line = 1; line <= n; line++)
+			_putchar('_');
+		_putchar('\n');
 	}
-	_putchar('\n')
 }
