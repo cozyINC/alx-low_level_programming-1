cd69fad78c229cd4438d172eee47504f91480918
Tijani Olanrewaju <tijanigbolahangbolahan32@gmail.com>
2022-09-16 11:18:22 -0700

    c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,4 @@
-#include "main.h"
-
+#include"main.h"
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -7,7 +6,6 @@
 
 int main(void)
 {
-
 	int i;
 
 	i = 98 % 2;
